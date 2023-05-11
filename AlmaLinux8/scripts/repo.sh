55d7834b1eabbbb90e38f536a71aca9d0cc9b3f3
alexmbarbosa <alexmendes.unix@gmail.com>
2023-05-11 02:35:48 +0100

    Edit repo script file

--- AlmaLinux8/scripts/repo.sh
+++ AlmaLinux8/scripts/repo.sh
@@ -2,16 +2,11 @@
 
 # Update the system
 dnf update -y
-
 # Install epel-release
 dnf install epel-release -y
-
-# Install rpm fusion (free)
-dnf install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm -y
-
-# Install rpm fusion (non-free)
-dnf install https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm -y
-
+# Install rpm fusion
+dnf install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
 # Clean the cache
 dnf clean all
 
