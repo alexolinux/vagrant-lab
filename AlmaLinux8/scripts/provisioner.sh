717ea70fb85bd58a586fc93a05bc1438ee3dec86
alexmbarbosa <alexmendes.unix@gmail.com>
2023-05-20 15:03:45 +0100

    Add net packages

--- AlmaLinux8/scripts/provisioner.sh
+++ AlmaLinux8/scripts/provisioner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add your Host Ip/Name below this line to your HOSTIP/HOSTALIAS environment variable.
-HOSTIP=
+HOSTIP=192.168.1.66
 HOSTALIAS='master'
 
 echo -e "1. Appending Hosts\n"
@@ -25,6 +25,8 @@ Packages=(
   "net-tools"
   "bind-utils"
   "tcpdump"
+  "nmap"
+  "nc"
   "iperf3"
   "jq"
 )
