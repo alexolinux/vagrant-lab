8d9fd33a4333166a259998eb7e27b69d02c0ed79
alexmbarbosa <alexmendes.unix@gmail.com>
2024-01-09 15:57:22 +0000

    Change hosts script

--- Linux/scripts/hosts.sh
+++ Linux/scripts/hosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 entries=(
-  "192.168.1.72   hypervisor   	hyper.local"
-  "192.168.1.111  vm1 vm1.local node1"
-  "192.168.1.112  vm2 vm2.local node2"
+  "192.168.0.2   hypervisor   	hyper.local"
+  "192.168.0.11  vm1 vm1.local node1"
+  "192.168.0.12  vm2 vm2.local node2"
 )
 
 for entry in "${entries[@]}"; do
